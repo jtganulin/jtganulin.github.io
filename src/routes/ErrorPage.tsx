4197f8016fd61c64e287b4a7547a13560db25913
Jeffrey Ganulin <jganulin6@gmail.com>
2024-03-12 15:58:48 -0400

    Improve ErrorPage

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import styles from "../styles/ErrorPage.module.css";
+import { Helmet } from "react-helmet";
 
-export default function ErrorPage({ error, resetErrorBoundary } : { error: Error, resetErrorBoundary: () => void }) {
+export default function ErrorPage({ error, resetErrorBoundary }: { error: Error, resetErrorBoundary?: () => void; }) {
     return (
         <>
-            <h1 className="pageHeading">Something went wrong</h1>
-            <p>
-                An error has occurred, try refreshing the page or head to the <Link to="/">home page</Link> to start over.
-            </p>
-            <pre>Error: {error?.message}</pre>
+            <Helmet>
+                <meta name="robots" content="noindex" />
+            </Helmet>
+            <main className={styles.errorPage}>
+                <h1 className="pageHeading">Something went wrong</h1>
+                <p>
+                    An error has occurred, whose details are below.
+                    <br />
+                    If refreshing the page doesn&apos;t work, you can head to the <Link to="/">home page</Link> or try again later.
+                </p>
+                <pre>{error?.stack ?? error?.message ?? "An unknown error occurred."}</pre>
+            </main>
         </>
     );
 }
