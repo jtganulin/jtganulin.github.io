00789fd2ab53ce5858adf106f20f625398aace35
Jeffrey Ganulin <jganulin6@gmail.com>
2024-03-18 19:12:58 -0400

    Fix Bio and Deploy Action caching

--- src/views/Bio.tsx
+++ src/views/Bio.tsx
@@ -11,12 +11,10 @@ export default function Bio() {
                 <p>
                     A strong team player, as well as an effective leader and independent contributor, I&apos;ve worked in a variety of roles across all phases of the software development lifecycle, from requirements gathering and analysis to design, development, testing, and deployment, all the while employing my attention to detail and ability to communicate effectively with both technical and non-technical stakeholders to deliver high-quality solutions wherever I am needed.
                     <br /><br />
-                    I&apos;ve worked with a wide variety of technologies on projects spanning multiple industry use cases, including finance, healthcare, and education, and have experience with both front-end and back-end development. I have a strong understanding of software engineering principles and best practices, including Agile methodologies. I&apos;m also passionate about open-source software and have contributed to a number of projects on GitHub, both in terms of code and documentation.
+                    I&apos;ve worked with a wide variety of technologies on projects spanning multiple industry use cases, including finance, healthcare, and education, and have experience with both front-end and back-end development. I have a strong understanding of software engineering principles and best practices, including Agile methodologies. I&apos;m also passionate about open-source software and have contributed to a number of projects on GitHub, both in terms of code and documentation. Improved diversity and inclusion in the tech industry is important to me, and I have contributed to initiatives promoting these values through open-source collaboration.
                 </p>
                 <h4>Personal Summary</h4>
                 <p>
-                    Outside of academic and employment pursuits, I&apos;m interested in open-source software, and have contributed to a few projects on GitHub, both in code and documentation. I also support diversity and inclusion, and have contributed to initiatives promoting these values in the tech industry.
-
                     Apart from my passion for technology, I&apos;m also a food enthusiast, and amateur chef and baker. I love reading, hiking, amd video games, as well as photography. I also enjoy traveling and learning about different cultures, and their cuisines and languages.
                 </p>
             </div>
