d94eeecd6461a08337846200c2084ac2d4187a49
Jeffrey Ganulin <jganulin6@gmail.com>
2024-04-01 16:24:35 -0400

    Add clarifying labels to Socials btns on Contact page

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,7 +3,7 @@ import type { IconName } from "@fortawesome/fontawesome-svg-core";
 
 import styles from "../styles/Socials.module.css";
 
-export default function Socials({ displayLocation = false }: { displayLocation?: boolean }) {
+export default function Socials({ displayLocation = false, expanded = false }: { displayLocation?: boolean, expanded?: boolean; }) {
     return (
         <div className={styles.socialsContainer}>
             {displayLocation && (
@@ -12,17 +12,15 @@ export default function Socials({ displayLocation = false }: { displayLocation?:
                 </div>
             )}
             <div className={styles.socials}>
-                {/* <div> */}
-                    <a title="View my GitHub profile" href="https://github.com/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-github" as IconName} /></a>
-                    <a title="View my LinkedIn profile" href="https://linkedin.com/in/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} /></a>
-                    {/*<a title="View my Twitter profile" href="#" className={styles.social} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={"fab fa-twitter" as 
-                    <a title="View my Instagram profile" href="#" className={styles.social} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={"fab fa-instagram" as IconName} /></a>
-                    <a title="View my Facebook profile" href="#" className={styles.social} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={"fab fa-facebook" as IconName} /></a> */}
-                    <a title="Send me an email" href="mailto:jganulin6@gmail.com" className={styles.social} rel="noreferrer, noindex, nofollow"><FontAwesomeIcon icon={"fa fa-envelope" as IconName} /></a>
-                {/* </div> */}
-                {/* <a title="View my YouTube profile" href="#" className={styles.social} target="_blank"><FontAwesomeIcon icon={"fab fa-youtube" as IconName} /></a>
-                <a title="View my Discord profile" href="#" className={styles.social} target="_blank"><FontAwesomeIcon icon={"fab fa-discord" as IconName} /></a>
-                <a title="View my Steam profile" href="#" className={styles.social} target="_blank"><FontAwesomeIcon icon={"fab fa-steam" as IconName} /></a> */}
+                <div>
+                    <a title="View my GitHub profile" href="https://github.com/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-github" as IconName} /> {expanded && "GitHub"}</a>
+                </div>
+                <div>
+                    <a title="View my LinkedIn profile" href="https://linkedin.com/in/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} /> {expanded && "LinkedIn"}</a>
+                </div>
+                <div>
+                    <a title="Send me an email" href="mailto:jganulin6@gmail.com" className={styles.social} rel="noreferrer, noindex, nofollow"><FontAwesomeIcon icon={"fa fa-envelope" as IconName} /> {expanded && "Email Me"}</a>
+                </div>
             </div>
         </div>
     );
