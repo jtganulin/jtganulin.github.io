e6c26f87fc949aaa49eb9233ea25979f8df64837
Jeffrey Ganulin <jganulin6@gmail.com>
2024-04-02 11:20:29 -0400

    Move homepage image to top bar

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -15,13 +15,6 @@ export default function Intro({ refer }: { refer?: React.RefObject<HTMLDivElemen
     return (
         <div ref={refer}>
             <div className={styles.heading}>
-                <img src="/images/Me.jpg" alt="Jeffrey Ganulin" width="200" height="200" style={{
-                    borderRadius: "50%",
-                    border: "2px solid var(--color-accent)",
-                    aspectRatio: "4 / 3",
-                    objectFit: "cover",
-                    marginBottom: "1rem",
-                }} />
                 <h2><em>Hello, I&apos;m</em></h2>
                 <h1>Jeffrey Ganulin</h1>
                 <Socials displayLocation={true} />
