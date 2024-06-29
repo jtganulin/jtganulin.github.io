56b3779e197bf6536670b6e7be4ff131031340d8
Jeffrey Ganulin <jganulin6@gmail.com>
2024-06-29 13:30:41 -0400

    Adjust Avatar styling to increase visibility

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 export default function Avatar({ width, height, style }: { width?: number | string; height?: number | string; style: object; }) {
     return (
         <img src="/images/Me.jpg" alt="Jeffrey Ganulin" width={width} height={height} style={{
-            borderRadius: "50%",
+            borderRadius: "1rem",
             border: "2px solid var(--color-accent)",
             aspectRatio: 1.3333,
             objectFit: "cover",
