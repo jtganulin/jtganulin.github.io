29f478036ba8640302a2aa2c0602169320ac2414
Jeffrey Ganulin <jganulin6@gmail.com>
2025-03-05 11:34:45 -0500

    Homepage improvements
    * Add button navigation between homepage sections
    * Adjust homepage layout so that the images appear only on larger screens, and the content is more prominent
    * Adjust "Scroll to Top" button; remove the label on mobile
    * Adjust theme colors and fonts
    * Improve link visibility
    * Ensure first homepage section's height fills the screen beneath the top nav bar

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,27 +1,65 @@
+import { memo, useCallback } from "react";
 import styles from "../styles/Home.module.css";
 
-const HomeSection = ({ id, bgIndex, children }: { id?: string; bgIndex: number; children: React.ReactNode; }) => {
+const SectionScrollButton = memo(({ targetID, label }:
+    {
+        targetID: "bio" | "education" | "experience" | "skills";
+        label: string;
+    }
+) => {
+    const scroll = useCallback(() => {
+        if (!targetID) return;
+
+        document.getElementById(targetID)?.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }, [targetID]);
+
+    if (!targetID) return null;
+
     return (
-        <section id={id}>
+        <div className={`${styles.sectionScrollButton}`}>
+            <button
+                onClick={scroll}
+                type="button"
+                aria-label={`Scroll to the ${targetID} section`}
+            >
+                {"↓ " + label}
+            </button>
+        </div>
+    );
+});
+
+const HomeSection = ({ id, sectionTitle, bgIndex, scrollBtnTarget, scrollBtnLabel, children }: {
+    id?: string;
+    sectionTitle?: string;
+    bgIndex: number;
+    scrollBtnTarget?: "bio" | "education" | "experience" | "skills";
+    scrollBtnLabel?: string;
+    children: React.ReactNode;
+}) => {
+    return (
+        <section id={id} className="homeSectionContainer">
             <div className={styles.homeSection}>
                 <div className={styles.homeSectionContent}>
+                    {sectionTitle && <h1 className={styles.sectionTitle}>{sectionTitle}</h1>}
                     {children}
                 </div>
+                {scrollBtnTarget && (
+                    <SectionScrollButton
+                        targetID={scrollBtnTarget}
+                        label={scrollBtnLabel || "View More"}
+                    />
+                )}
             </div>
             <div className={styles.bgContainer}>
                 <img src={`/images/Home/${bgIndex}.jpg`} alt={`Homepage background ${bgIndex}`}
-                    width="1500" height="2000"
-                    style={{
-                        maxWidth: "100%",
-                        height: "100%",
-                        objectFit: "fill",
-                        objectPosition: "center",
-                        aspectRatio: 1.3333
-                    }}
+                    width="1500" height="2000" loading="lazy"
                 />
             </div>
         </section>
     );
 };
 
-export default HomeSection;
+export default memo(HomeSection);
