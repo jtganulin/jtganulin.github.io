d8a6ef89adb1e016a0356280be82d9f51e2c1b09
Jeffrey Ganulin <jganulin6@gmail.com>
2025-03-05 12:28:08 -0500

    Add LoadingPage spinner

--- src/routes/LoadingPage.tsx
+++ src/routes/LoadingPage.tsx
@@ -3,13 +3,12 @@ import { ThemeContext } from "../providers/ThemeProvider";
 import styles from "../styles/LoadingPage.module.css";
 
 export default function LoadingPage() {
-    // TODO: Add spinner and ease in/out animation
-
     const { theme } = useContext(ThemeContext);
     return (
         <div className={styles.loading} data-theme={theme}>
-            <h1>Loading...</h1>
+            <h1>Loading</h1>
             <p>Just a moment...</p>
+            <div className={styles.spinner}></div>
         </div>
     );
 }
