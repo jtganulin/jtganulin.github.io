99f4d3553a2dd2f5cbe5789322b0eace346bf97c
Jeffrey Ganulin <jganulin6@gmail.com>
2025-03-05 12:27:38 -0500

    Reformat Socials.tsx; Remove location section from Socials and fix link properties; Add "view resume" button to Socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,26 +1,23 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { IconName } from "@fortawesome/fontawesome-svg-core";
-
 import styles from "../styles/Socials.module.css";
 
-export default function Socials({ displayLocation = false, expanded = false }: { displayLocation?: boolean, expanded?: boolean; }) {
+export default function Socials({ expanded = false }: { expanded?: boolean; }) {
     return (
         <div className={styles.socialsContainer}>
-            {displayLocation && (
-                <div className={styles.location}>
-                    <FontAwesomeIcon icon={"fa fa-map-pin" as IconName} color="red" /><span>&nbsp;Atlanta, GA</span>
-                </div>
-            )}
-            <div className={styles.socials}>
-                <div>
-                    <a title="View my GitHub profile" href="https://github.com/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-github" as IconName} /> {expanded && "GitHub"}</a>
-                </div>
-                <div>
-                    <a title="View my LinkedIn profile" href="https://linkedin.com/in/jtganulin" className={styles.social} target="_blank" rel="noopener, noreferrer"><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} /> {expanded && "LinkedIn"}</a>
-                </div>
-                <div>
-                    <a title="Send me an email" href="mailto:jganulin6@gmail.com" className={styles.social} rel="noreferrer, noindex, nofollow"><FontAwesomeIcon icon={"fa fa-envelope" as IconName} /> {expanded && "Email Me"}</a>
-                </div>
+            <div>
+                <a href="https://github.com/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my GitHub profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-github" as IconName} /> {expanded && "GitHub"}</a>
+            </div>
+            <div>
+                <a href="https://linkedin.com/in/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my LinkedIn profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} /> {expanded && "LinkedIn"}</a>
+            </div>
+            <div>
+                <a href="mailto:jganulin6@gmail.com" target="_blank" rel="noopener nofollow noreferrer" title="Send me an email" className={styles.social}><FontAwesomeIcon icon={"fa fa-envelope" as IconName} /> {expanded && "Email Me"}</a>
+            </div>
+            <div>
+                <a href="/JeffreyGanulinResume.pdf" target="_blank" rel="noopener" title="View my resume" className={styles.social}>
+                    Resume
+                </a>
             </div>
         </div>
     );
