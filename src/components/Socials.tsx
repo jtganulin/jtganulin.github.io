0a84c863f0d11c0c40f3fda9e64efb44dfb54262
Jeffrey Ganulin <jganulin6@gmail.com>
2025-03-24 09:00:34 -0400

    * Make skill badge and social icons' widths more consistent

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -6,13 +6,13 @@ export default function Socials({ expanded = false }: { expanded?: boolean; }) {
     return (
         <div className={styles.socialsContainer}>
             <div>
-                <a href="https://github.com/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my GitHub profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-github" as IconName} /> {expanded && "GitHub"}</a>
+                <a href="https://github.com/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my GitHub profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-github" as IconName} fixedWidth /> {expanded && "GitHub"}</a>
             </div>
             <div>
-                <a href="https://linkedin.com/in/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my LinkedIn profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} /> {expanded && "LinkedIn"}</a>
+                <a href="https://linkedin.com/in/jtganulin" target="_blank" rel="noopener nofollow noreferrer" title="View my LinkedIn profile" className={styles.social}><FontAwesomeIcon icon={"fab fa-linkedin" as IconName} fixedWidth /> {expanded && "LinkedIn"}</a>
             </div>
             <div>
-                <a href="mailto:jganulin6@gmail.com" target="_blank" rel="noopener nofollow noreferrer" title="Send me an email" className={styles.social}><FontAwesomeIcon icon={"fa fa-envelope" as IconName} /> {expanded && "Email Me"}</a>
+                <a href="mailto:jganulin6@gmail.com" target="_blank" rel="noopener nofollow noreferrer" title="Send me an email" className={styles.social}><FontAwesomeIcon icon={"fa fa-envelope" as IconName} fixedWidth /> {expanded && "Email Me"}</a>
             </div>
             <div>
                 <a href="/JeffreyGanulinResume.pdf" target="_blank" rel="noopener" title="View my resume" className={styles.social}>
